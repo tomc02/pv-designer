d379c78bbdfb41ca0a9f9b70bafad95106cba8e9
Tomas Cienciala <cie0122@vsb.cz>
2023-08-11 16:58:07 +0200

    Added map picure save

--- pv_designer/web_pv_designer/static/js/utils_other.js
+++ pv_designer/web_pv_designer/static/js/utils_other.js
@@ -39,3 +39,24 @@ function getPower(shapes, kwPerPanel){
     });
     return panelsCount * kwPerPanel;
 }
+
+function getMapPicture() {
+    //hide the map controls (zoom, street view, drawing manager, etc.)
+    map.setOptions({zoomControl: false, streetViewControl: false, mapTypeControl: false, fullscreenControl: false});
+    drawingManager.setOptions({drawingControl: false});
+    map.setOptions({styles: [{featureType: "all", elementType: "labels", stylers: [{visibility: "off"}]}]});
+    setTimeout(function () {
+        //take the screenshot
+        html2canvas(document.querySelector('#map'), {
+            backgroundColor: null,
+            useCORS: true
+        }).then(canvas => {
+            //show the map controls again
+            map.setOptions({zoomControl: true, streetViewControl: true, mapTypeControl: true});
+            //add the screenshot to the page
+            document.body.appendChild(canvas);
+            //save the screenshot
+            console.log(canvas.toDataURL());
+        });
+    }, 200);
+}
