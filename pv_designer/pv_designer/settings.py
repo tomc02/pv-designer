847f1c888a76c50bf1a9e14a2999087c7bc89d68
Tomas Cienciala <tomas.cienciala.st1@vsb.cz>
2024-02-25 11:44:31 +0100

    Init migration

--- pv_designer/pv_designer/settings.py
+++ pv_designer/pv_designer/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'web_pv_designer.apps.WebPvDesignerConfig',
-    'django.contrib.sites',  # Add this if it's not already there
+    'django.contrib.sites',
+    'django.contrib.gis',
     'allauth',
     'allauth.account',
     'django_extensions',
@@ -81,11 +82,12 @@ WSGI_APPLICATION = 'pv_designer.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.contrib.gis.db.backends.spatialite',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
+SPATIALITE_LIBRARY_PATH = 'mod_spatialite.so'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
--- pv_designer/web_pv_designer/models.py
+++ pv_designer/web_pv_designer/models.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.models import AbstractUser
-from django.db import models
+from django.contrib.gis.db import models
 
 
 class CustomUser(AbstractUser):
     address = models.CharField(max_length=255, blank=True, null=True)
+    home_location = models.PointField(blank=True, null=True)
 
     def __str__(self):
         return self.username
