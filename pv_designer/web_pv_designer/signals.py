ff263043507604d97aacc0e71839066c4f24215f
Tomas Cienciala <tomas.cienciala.st1@vsb.cz>
2024-02-27 23:46:09 +0100

    Sigals

--- pv_designer/web_pv_designer/signals.py
+++ pv_designer/web_pv_designer/signals.py
@@ -4,10 +4,6 @@ from django.dispatch import receiver, Signal
 
 solar_panel_added = Signal()
 
-@receiver(password_changed)
-def password_change_success(request, **kwargs):
-    messages.success(request, 'Your password has been changed successfully.')
-
 @receiver(solar_panel_added)
 def solar_panel_added_receiver(sender, instance, request, **kwargs):
     messages.success(request, f'New solar panel {instance.model} added successfully!')
