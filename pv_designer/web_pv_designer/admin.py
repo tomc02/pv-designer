a70680e7ec8157b2d807fe6d83338aab591f86a5
Tomas Cienciala <tomas.cienciala.st1@vsb.cz>
2024-04-24 19:42:06 +0200

    Adapt to newer versions of Allauth and Django

--- pv_designer/web_pv_designer/admin.py
+++ pv_designer/web_pv_designer/admin.py
@@ -9,7 +9,7 @@ admin.site.index_title = 'Welcome to PV Designer Portal'
 admin.site.register(CustomUser)
 admin.site.register(PVPowerPlant)
 admin.site.register(PVSystemDetails)
-admin.site.register(Area, admin.OSMGeoAdmin)
+admin.site.register(Area)
 admin.site.register(SolarPanel)
 admin.site.register(MonthlyConsumption)
 
